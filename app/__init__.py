c8b133a97491edf077609af2a56458d835d9ce76
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-08-03 12:28:30 +0100

    __ini__ fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
-from app.src.orchestration.integrate_web_search import orchestrated_codegen
+from app.src.orchestration.integrate_web_search import integrate_web_search
 from app.src.agents.brainstormer.brainstormer import BrainstormerAgent
 from app.src.agents.code_gen.code_gen import CodeGenAgent
 from app.src.agents.web_searcher.web_searcher import WebSearcherAgent
 from app.utils.ascii_art import ASCII_ART
 
 __all__ = [
-    "orchestrated_codegen",
+    "integrate_web_search",
     "BrainstormerAgent", 
     "CodeGenAgent",
     "WebSearcherAgent",
--- app/src/orchestration/__init__.py
+++ app/src/orchestration/__init__.py
@@ -1,6 +1,5 @@
-from app.src.orchestration.integrate_web_search import orchestrated_codegen, integrate_web_search
+from app.src.orchestration.integrate_web_search import integrate_web_search
 
 __all__ = [
-    "orchestrated_codegen",
     "integrate_web_search"
 ]
